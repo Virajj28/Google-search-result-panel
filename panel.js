d9a79d7b87b12debac64377ce5b4f68cb0a52c79
Viraj Jadhav <viraj28j@gmail.com>
2021-08-23 20:02:51 +0530

    first commit

---
+++ panel.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("container");
+    if ( === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
